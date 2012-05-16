7d5fe2d7d03e9c8e1d57a3436b5e98ceb2dd16ea
Dmitry Monakhov <dmonakhov@gmail.com>
2012-05-16 16:40:51 +0400

    initial commit

---
+++ src/com/google/libjingle/example/talk.java
@@ -0,0 +1,15 @@
+package com.google.libjingle.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class talk extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
